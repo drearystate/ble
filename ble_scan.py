bba1c21b8794fffe4e3c146c41cef9443f597803
drearystate <drearystate@hotmail.com>
2023-07-26 12:15:30 -0500

    Update ble_scan.py

--- ble_scan.py
+++ ble_scan.py
@@ -1,8 +1,12 @@
+import os
+import csv
 import asyncio
 from bleak import BleakScanner
-from datetime import datetime
-import pymysql
+from datetime import datetime, timedelta
 import time
+import logging
+import pymysql
+import requests
 import subprocess
 
 # Add MySQL database credentials
@@ -11,66 +15,37 @@ db_user = 'blenextinqu_drearystate'
 db_password = 'Silverbook224!'
 db_name = 'blenextinqu_devices'
 
-# Function to get Raspberry Pi's serial number (which will be used as hostname)
-def get_serial():
-    try:
-        cpuinfo = subprocess.run(['cat', '/proc/cpuinfo'], capture_output=True, text=True).stdout
-        for line in cpuinfo.split("\n"):
-            if line.startswith('Serial'):
-                serial = line.split(":")[1].strip()
-                return serial
-    except:
-        return "ERROR"
-    
-# Set hostname
-hostname = get_serial()
+git_repo = 'https://github.com/drearystate/ble.git'
+
+# Get the hostname (which is the Raspberry Pi's serial)
+hostname = subprocess.check_output(['cat', '/proc/cpuinfo', '|', 'grep', 'Serial', '|', 'cut', '-d', ' ', '-f', '2']).decode().strip()
+
+def check_for_updates():
+    # Check for updates every 6 hours
+    response = requests.get(git_repo)
+    if response.status_code == 200:
+        with open(__file__ + '.new', 'wb') as f:
+            f.write(response.content)
+        os.rename(__file__, __file__ + '.old')  # rename the current script
+        os.rename(__file__ + '.new', __file__)  # rename the new script to the old script's name
+        os.execv(__file__, [])  # restart the script
 
 # Function to write data to the MySQL database
-def write_to_mysql(timestamp, device_address, rssi, device_name, metadata, hostname):
-    try:
-        # Connect to the database
-        connection = pymysql.connect(host=db_host, user=db_user, password=db_password, database=db_name)
-
-        # Create a cursor to interact with the database
-        cursor = connection.cursor()
-
-        # SQL query to insert data into the database
-        insert_query = """INSERT INTO devices (timestamp, device_address, rssi, device_name, metadata, device_hostname) 
-                          VALUES (%s, %s, %s, %s, %s, %s)"""
-
-        # Data to insert
-        data = (timestamp, device_address, rssi, device_name, str(metadata), hostname)
-
-        # Execute the SQL query with the data
-        cursor.execute(insert_query, data)
-
-        # Commit the changes to the database
-        connection.commit()
-
-    except Exception as e:
-        print(f"Error inserting to MySQL: {e}")
-
-    finally:
-        # Close the database connection
-        if connection:
-            connection.close()
+def write_to_mysql(device):
+    connection = pymysql.connect(host=db_host, user=db_user, password=db_password, database=db_name)
+    cursor = connection.cursor()
+    insert_query = """INSERT INTO devices (device_hostname, device_address, device_name, rssi, timestamp) VALUES (%s, %s, %s, %s, %s)"""
+    cursor.execute(insert_query, (hostname, device.address, device.name, device.rssi, datetime.now()))
+    connection.commit()
+    connection.close()
 
 async def discover():
-    # Initialize the BleakScanner
     scanner = BleakScanner()
-
-    # Discover devices
     devices = await scanner.discover()
-
     for device in devices:
-        timestamp = datetime.now()
-        device_address = device.address
-        rssi = device.rssi
-        device_name = device.name
-        metadata = device.metadata
-        write_to_mysql(timestamp, device_address, rssi, device_name, metadata, hostname)
+        write_to_mysql(device)
 
-# Run the device discovery in a loop
 while True:
+    check_for_updates()
     asyncio.run(discover())
     time.sleep(60)
